2925115c812db7c39665036152ff6a1adf8e6b55
ShotSkydiver <bigglesworth330@gmail.com>
2020-11-10 13:05:08 -0700

    bump version to 0.1.7

--- ios/FlexDebugging.m
+++ ios/FlexDebugging.m
@@ -7,30 +7,38 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(showExplorer)
 {
+#if DEBUG
 dispatch_async(dispatch_get_main_queue(), ^{
     [[FLEXManager sharedManager] showExplorer];
 });
+#endif
 }
 
 RCT_EXPORT_METHOD(hideExplorer)
 {
+#if DEBUG
 dispatch_async(dispatch_get_main_queue(), ^{
     [[FLEXManager sharedManager] hideExplorer];
 });
+#endif
 }
 
 RCT_EXPORT_METHOD(toggleExplorer)
 {
+#if DEBUG
 dispatch_async(dispatch_get_main_queue(), ^{
     [[FLEXManager sharedManager] toggleExplorer];
 });
+#endif
 }
 
 RCT_EXPORT_METHOD(setNetworkLogging)
 {
+#if DEBUG
 dispatch_async(dispatch_get_main_queue(), ^{
     [[FLEXManager sharedManager] setNetworkDebuggingEnabled:true];
 });
+#endif
 }
 
 @end
